e8159539baf3671fe6ed5797015503fa6ff6f9e2
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-10-24 18:16:15 +0200

    Add ProductRepository and DietaryRequirementRepository

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/DietaryRequirementRepository.cs
@@ -0,0 +1,17 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Core.Infrastructure;
+using Imi.Project.Api.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Infrastructure.Repositories
+{
+    public class DietaryRequirementRepository: BaseRepository<DietaryRequirement>, IDietaryRequirementRepository
+    {
+        public DietaryRequirementRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+
+        }
+    }
+}
