6030a40e3bbfd1e91a5e60be166d231e38bb6cf0
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-10-25 14:11:41 +0200

    Add search method

--- src/Imi.Project.Api/Controllers/SearchController.cs
+++ src/Imi.Project.Api/Controllers/SearchController.cs
@@ -1,4 +1,6 @@
-﻿using Imi.Project.Api.Core.Infrastructure;
+﻿using Imi.Project.Api.Core.DTO_S.Categories;
+using Imi.Project.Api.Core.DTO_S.Products;
+using Imi.Project.Api.Core.Infrastructure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -23,6 +25,24 @@ namespace Imi.Project.Api.Controllers
             _productRepository = productRepository;
             _categoryRepository = categoryRepository;
             _dietaryRequirementRepository = dietaryRequirementRepository;
+        }
+
+        public async Task<IActionResult> Search([FromQuery] string searchString)
+        {
+            var results = await _productRepository.SearchAsync(searchString);
+
+            var searchResultsDTO = results.Select(s => new ProductResponseDTO
+            {
+                Id = s.Id,
+                Name = s.Name,
+                Category = new CategoryResponseDTO
+                {
+                    Id = s.Category.Id,
+                    Name = s.Category.Name
+                }
+            });
+
+            return Ok(searchResultsDTO);
         }
     }
 }
