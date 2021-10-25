d44416f2301568b5e05b39dc9b5f3bf602db058e
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-10-25 14:30:05 +0200

    Add Request DTO's

---
+++ src/Imi.Project.Api.Core/DTO'S/DietaryRequirements/DietaryRequirementRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using Imi.Project.Api.Core.DTO_S.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Imi.Project.Api.Core.DTO_S.DietaryRequirements
+{
+    public class DietaryRequirementRequestDTO : BaseDTO
+    {
+        [Required(ErrorMessage ="Naam is verplicht")]
+        public string Name { get; set; }
+    }
+}
---
+++ src/Imi.Project.Api.Core/DTO'S/Products/ProductRequestDTO.cs
@@ -0,0 +1,25 @@
+﻿using Imi.Project.Api.Core.DTO_S.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Imi.Project.Api.Core.DTO_S.Products
+{
+    public class ProductRequestDTO : BaseDTO
+    {
+        [Required(ErrorMessage ="Naam is verplicht")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Beschrijving is verplicht")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage ="Prijs is verplicht")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage ="Categorie is verplicht")]
+        public Guid CategoryId { get; set; }
+
+        public Guid DietaryRequirementId { get; set; }
+    }
+}
