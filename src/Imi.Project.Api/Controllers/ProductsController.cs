625ce57a41a47cde3e04f8e0bb9a315785678ac1
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-10-25 09:40:08 +0200

    Add products and requirements controller

---
+++ src/Imi.Project.Api/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Imi.Project.Api.Core.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        protected readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+    }
+}
