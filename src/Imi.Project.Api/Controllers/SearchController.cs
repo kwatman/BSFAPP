4ae58d0cd44dbb3ce35c37805f1760999faa3927
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-10-25 10:16:00 +0200

    Add SearchController

---
+++ src/Imi.Project.Api/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using Imi.Project.Api.Core.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IDietaryRequirementRepository _dietaryRequirementRepository;
+
+        public SearchController(IProductRepository productRepository, 
+            ICategoryRepository categoryRepository,
+            IDietaryRequirementRepository dietaryRequirementRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+            _dietaryRequirementRepository = dietaryRequirementRepository;
+        }
+    }
+}
