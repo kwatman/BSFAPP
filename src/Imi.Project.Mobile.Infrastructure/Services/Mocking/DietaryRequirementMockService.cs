ad72a321204168ea411c27b5684d314178412735
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-10-27 13:06:02 +0200

    Add DietaryRequirementMockService

--- src/Imi.Project.Mobile.Infrastructure/Services/Mocking/DietaryRequirementMockService.cs
+++ src/Imi.Project.Mobile.Infrastructure/Services/Mocking/DietaryRequirementMockService.cs
@@ -1,33 +1,32 @@
 ﻿using Imi.Project.Mobile.Core.Models;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Imi.Project.Mobile.Infrastructure.Services.Mocking
 {
-    public class CategoryMockService: BaseMockService<Category>, ICategoryService
+    public class DietaryRequirementMockService : BaseMockService<DietaryRequirement>, IDietaryRequirementService
     {
-        private static List<Category> categories = new List<Category>
+        private static List<DietaryRequirement> dietaryRequirements = new List<DietaryRequirement>
         {
-            new Category
+            new DietaryRequirement
             {
                 Id = Guid.Parse("00000000-0000-0000-0000-000000000001"),
-                Name = "Taart"
+                Name = "Vegan"
             },
-            new Category
+            new DietaryRequirement
             {
                 Id = Guid.Parse("00000000-0000-0000-0000-000000000002"),
-                Name = "Gebak"
+                Name = "Glutenvrij"
             },
-            new Category
+            new DietaryRequirement
             {
                 Id = Guid.Parse("00000000-0000-0000-0000-000000000003"),
-                Name = "Koekjes met glazuur"
+                Name = "Notenvrij"
             },
-            new Category
+            new DietaryRequirement
             {
                 Id = Guid.Parse("00000000-0000-0000-0000-000000000004"),
-                Name = "Koekjes met rolfondant"
+                Name = "Lactosevrij"
             }
         };
     }
