ced3dba6cc351d71e4a552e9957b252051bd6935
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-10-28 10:04:52 +0200

    Add RequestDTO's

---
+++ src/Imi.Project.Api.Core/DTO'S/BlogPosts/BlogPostRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using Imi.Project.Api.Core.DTO_S.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Imi.Project.Api.Core.DTO_S.BlogPosts
+{
+    public class BlogPostRequestDTO : BaseDTO
+    {
+        [Required(ErrorMessage = "Titel is verplicht")]
+        public string Title { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+}
---
+++ src/Imi.Project.Api.Core/DTO'S/Users/UserRequestDTO.cs
@@ -0,0 +1,26 @@
+﻿using Imi.Project.Api.Core.DTO_S.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Imi.Project.Api.Core.DTO_S.Users
+{
+    public class UserRequestDTO : BaseDTO
+    {
+        [Required(ErrorMessage = "Naam is verplicht")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Voornaam is verplicht")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Email is verplicht")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Telefoonnummer is verplicht")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Wachtwoord is verplicht")]
+        public string Password { get; set; }
+    }
+}
