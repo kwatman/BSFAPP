77fccc9bde362650e240c335d9a17ff633a8cd69
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-10-28 09:05:32 +0200

    Add UserRepository

--- src/Imi.Project.Api.Infrastructure/Repositories/UserRepository.cs
+++ src/Imi.Project.Api.Infrastructure/Repositories/UserRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Imi.Project.Api.Core.Entities;
 using Imi.Project.Api.Core.Infrastructure;
 using Imi.Project.Api.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,7 +19,11 @@ namespace Imi.Project.Api.Infrastructure.Repositories
 
         public async Task<IEnumerable<User>> SearchAsync(string searchString)
         {
+            var users = await GetAll().Where(p => p.Name.Contains(searchString.Trim().ToUpper())
+            || p.Name.Contains(searchString.Trim().ToUpper())
+            || p.Surname.Contains(searchString.Trim().ToUpper())).ToListAsync();
 
+            return users;
         }
     }
 }
