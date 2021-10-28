ee0ce513d0e13fdd8e9d66b71e76ecee0053970f
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-10-28 09:24:04 +0200

    Add Response DTO's

---
+++ src/Imi.Project.Api.Core/DTO'S/BlogPosts/BlogPostResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using Imi.Project.Api.Core.DTO_S.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.DTO_S.BlogPosts
+{
+    public class BlogPostResponseDTO : BaseDTO
+    {
+        public string Title { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+}
---
+++ src/Imi.Project.Api.Core/DTO'S/Users/UserResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using Imi.Project.Api.Core.DTO_S.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.DTO_S.Users
+{
+    public class UserResponseDTO : BaseDTO
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
