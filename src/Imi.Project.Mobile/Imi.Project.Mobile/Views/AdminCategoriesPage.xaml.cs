6c4522c76d5e5bb53decd4760473f96b028af473
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-11-03 17:08:47 +0100

    Reformat ProductsPage and add CategoriesPage

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Views/AdminCategoriesPage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Views/AdminCategoriesPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Imi.Project.Mobile.Core.Models;
+using Imi.Project.Mobile.Infrastructure.Services;
+using Imi.Project.Mobile.Infrastructure.Services.MockData;
+using Imi.Project.Mobile.Infrastructure.Services.Mocking;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +16,37 @@ namespace Imi.Project.Mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AdminCategoriesPage : ContentPage
     {
+        private readonly ICategoryService categoryService;
         public AdminCategoriesPage()
         {
             InitializeComponent();
+
+            categoryService = new CategoryMockService();
+        }
+        private void ShowCategories()
+        {
+            var categories = categoryService.GetAll(context: CategoryMockData.categoryData);
+            lvCategories.ItemsSource = categories;
+        }
+
+        protected override void OnAppearing()
+        {
+            ShowCategories();
+            base.OnAppearing();
+        }
+        private async void CategoryUpdate_Clicked(object sender, EventArgs e)
+        {
+            var selectedCategory = ((MenuItem)sender).CommandParameter as Category;
+            await DisplayAlert("Edit", $"Editing {selectedCategory.Name}", "OK");
+            await Navigation.PushAsync(new AdminCategoriesUpdatePage());
+
+        }
+
+        private async void CategoryDelete_Clicked(object sender, EventArgs e)
+        {
+            var selectedCategory = ((MenuItem)sender).CommandParameter as Category;
+            await categoryService.Delete(CategoryMockData.categoryData, selectedCategory.Id);
+            ShowCategories();
         }
     }
 }
---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Views/AdminCategoriesUpdatePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Imi.Project.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AdminCategoriesUpdatePage : ContentPage
+    {
+        public AdminCategoriesUpdatePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
