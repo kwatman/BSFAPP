b8cb18c05cb8381cf82722717f95c4506e4abaa1
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-11-13 17:20:38 +0100

    Add CacheService class

---
+++ src/Imi.Project.Mobile.Core/Services/CacheService.cs
@@ -0,0 +1,17 @@
+﻿using Akavache;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Mobile.Core.Services
+{
+    public class CacheService
+    {
+        protected IBlobCache _cache;
+
+        public BaseService(IBlobCache cache)
+        {
+            _cache = cache ?? BlobCache.LocalMachine;
+        }
+    }
+}
