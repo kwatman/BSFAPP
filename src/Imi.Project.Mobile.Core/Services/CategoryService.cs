3d898ec3b8a49b61776e32fc2327e7ca5b49e2cf
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-11-14 14:24:03 +0100

    Add CategoryService

---
+++ src/Imi.Project.Mobile.Core/Services/CategoryService.cs
@@ -0,0 +1,19 @@
+﻿using Akavache;
+using Imi.Project.Mobile.Core.Interfaces.IRepositories;
+using Imi.Project.Mobile.Core.Interfaces.IServices;
+using Imi.Project.Mobile.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Mobile.Core.Services
+{
+    public class CategoryService: BaseService<Category>, ICategoryService
+    {
+        protected readonly IBaseRepository _baseRepository;
+        public CategoryService(IBaseRepository baseRepository, IBlobCache cache = null) : base(null, cache)
+        {
+            _baseRepository = baseRepository;
+        }
+    }
+}
