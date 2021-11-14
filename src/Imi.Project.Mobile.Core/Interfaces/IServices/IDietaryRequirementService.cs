c27e6adb79b4daa388541d1b11d69c6950289e1f
Decru Amaury <71766425+AmauryDecru@users.noreply.github.com>
2021-11-14 14:30:53 +0100

    Add DietaryRequirmentService

---
+++ src/Imi.Project.Mobile.Core/Interfaces/IServices/IDietaryRequirementService.cs
@@ -0,0 +1,11 @@
+﻿using Imi.Project.Mobile.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Mobile.Core.Interfaces.IServices
+{
+    public interface IDietaryRequirementService: IBaseService<DietaryRequirement>
+    {
+    }
+}
