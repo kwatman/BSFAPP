8e842a23e3ab4ab03b84dfd8425b79bc2fc9e6c7
Decru Amaury <amaury.decru@student.howest.be>
2021-12-22 15:00:26 +0100

    Add BusinessLogic to CategoryService

--- src/Imi.Project.Api.Core/Services/CategoryService.cs
+++ src/Imi.Project.Api.Core/Services/CategoryService.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Core.Interfaces;
+using Imi.Project.Api.Core.Interfaces.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Services
 {
-    internal class CategoryService
+    public class CategoryService: ICategoryService
     {
+        protected readonly IBaseRepository<Category> _categoryRepository;
+
+        public CategoryService(IBaseRepository<Category> categoryService)
+        {
+            _categoryRepository = categoryService;
+        }
+
+        public IQueryable<Category> GetAll()
+        {
+            return _categoryRepository.GetAll();
+        }
+
+        public async Task<IEnumerable<Category>> ListAllAsync()
+        {
+            return await _categoryRepository.GetAll().ToListAsync();
+        }
+
+        public async Task<Category> GetByIdAsync(Guid id)
+        {
+            return await _categoryRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            return await _categoryRepository.UpdateAsync(category);
+        }
+
+        public async Task<Category> AddAsync(Category category)
+        {
+            return await _categoryRepository.AddAsync(category);
+        }
+
+        public async Task<Category> DeleteAsync(Category category)
+        {
+            return await _categoryRepository.DeleteAsync(category);
+        }
     }
 }
