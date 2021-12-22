f73b3d151527d6c24047307b509b68645b59482c
Decru Amaury <amaury.decru@student.howest.be>
2021-12-22 15:28:54 +0100

    Fix Service Interfaces

--- src/Imi.Project.Api.Core/Interfaces/Services/IDietaryRequirementService.cs
+++ src/Imi.Project.Api.Core/Interfaces/Services/IDietaryRequirementService.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Imi.Project.Api.Core.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Interfaces.Services
 {
     public interface IDietaryRequirementService
     {
+        IQueryable<DietaryRequirement> GetAll();
+        Task<IEnumerable<DietaryRequirement>> ListAllAsync();
+        Task<DietaryRequirement> GetByIdAsync(Guid id);
+        Task<DietaryRequirement> UpdateAsync(DietaryRequirement dietaryRequirement);
+        Task<DietaryRequirement> AddAsync(DietaryRequirement dietaryRequirement);
+        Task<DietaryRequirement> DeleteAsync(DietaryRequirement dietaryRequirement);
     }
 }
--- src/Imi.Project.Api.Core/Interfaces/Services/IUserService.cs
+++ src/Imi.Project.Api.Core/Interfaces/Services/IUserService.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Imi.Project.Api.Core.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Interfaces.Services
 {
     public interface IUserService
     {
+        IQueryable<User> GetAll();
+        Task<IEnumerable<User>> ListAllAsync();
+        Task<User> GetByIdAsync(Guid id);
+        Task<User> UpdateAsync(User user);
+        Task<User> AddAsync(User user);
+        Task<User> DeleteAsync(User user);
     }
 }
