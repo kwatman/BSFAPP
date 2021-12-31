0e9d7717a8600f380767f20bb549e1399489663b
Decru Amaury <amaury.decru@student.howest.be>
2021-12-31 15:31:57 +0100

    Add RegisterAsyncMethod in UsersController

--- src/Imi.Project.Api/Controllers/UsersController.cs
+++ src/Imi.Project.Api/Controllers/UsersController.cs
@@ -21,114 +21,25 @@ namespace Imi.Project.Api.Controllers
             _userService = userService;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            var users = await _userService.ListAllAsync();
-            var usersDTO = users.Select(u => new UserResponseDTO
-            {
-                Id = u.Id,
-                Name = u.Name,
-                Surname = u.Surname,
-                Email = u.Email,
-                PhoneNumber = u.PhoneNumber
-            });
-
-            return Ok(usersDTO);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(Guid id)
-        {
-            var user = await _userService.GetByIdAsync(id);
-
-            if(user == null)
-            {
-                return NotFound($"Geen gebruiker met id {id} gevonden");
-            }
-            else
-            {
-                var userDTO = new UserResponseDTO
-                {
-                    Id = user.Id,
-                    Name = user.Name,
-                    Surname = user.Surname,
-                    Email = user.Email,
-                    PhoneNumber = user.PhoneNumber
-                };
-
-                return Ok(userDTO);
-            }
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Add(UserRequestDTO userDTO)
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterAsync([FromBody]UserRequestDTO request)
         {
             if (!ModelState.IsValid)
             {
-                return BadRequest(ModelState.Values);
+                return BadRequest("Sommige velden zijn niet geldig!");
             }
             else
             {
-                var userToAdd = new User
+                var result = await _userService.RegisterAsync(request);
+
+                if (result.IsSuccess)
                 {
-                    Name = userDTO.Name,
-                    Surname = userDTO.Surname,
-                    Email = userDTO.Email,
-                    PhoneNumber = userDTO.PhoneNumber,
-                    Password = userDTO.Password
-                };
-
-                await _userService.AddAsync(userToAdd);
-
-                return Ok();
-            }
-        }
-
-        [HttpPut]
-        public async Task<IActionResult> Update(UserRequestDTO userDTO)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState.Values);
-            }
-            else
-            {
-                var userToUpdate = await _userService.GetByIdAsync(userDTO.Id);
-
-                if (userToUpdate == null)
-                {
-                    return NotFound($"Geen gebruiker met id {userDTO.Id} gevonden");
+                    return Ok(result);
                 }
                 else
                 {
-                    userToUpdate.Name = userDTO.Name;
-                    userToUpdate.Surname = userDTO.Surname;
-                    userToUpdate.Email = userDTO.Email;
-                    userToUpdate.PhoneNumber = userDTO.PhoneNumber;
-                    userToUpdate.Password = userDTO.Password;
-
-                    await _userService.UpdateAsync(userToUpdate);
-
-                    return Ok();
+                    return BadRequest(result);
                 }
-            }
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            var userToDelete = await _userService.GetByIdAsync(id);
-
-            if (userToDelete == null)
-            {
-                return NotFound($"Geen gebruiker met id {id} gevonden");
-            }
-            else
-            {
-                await _userService.DeleteAsync(userToDelete);
-
-                return Ok();
             }
         }
     }
