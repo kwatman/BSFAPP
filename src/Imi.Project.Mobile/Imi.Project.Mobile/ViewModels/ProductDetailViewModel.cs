d95bb77d567038d5f8c5e962d0b50a96899e67e6
AmauryDecru <amaury.decru@student.howest.be>
2022-01-07 16:04:01 -0800

    Add ProductsViewModel and ListExtension Method

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Mobile.ViewModels
+{
+    public class ProductDetailViewModel: FreshBasePageModel
+    {
+    }
+}
---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using FreshMvvm;
+using Imi.Project.Mobile.Core.Extensions;
+using Imi.Project.Mobile.Core.Interfaces.IServices;
+using Imi.Project.Mobile.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Mobile.ViewModels
+{
+    public class ProductsViewModel: FreshBasePageModel
+    {
+        private IProductService _productService;
+
+        private ObservableCollection<Product> _products;
+
+        public ProductsViewModel(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public ObservableCollection<Product> Products
+        {
+            get => _products;
+            set
+            {
+                _products = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private void OnProductTapped(Product selectedProduct)
+        {
+            CoreMethods.PushPageModel<ProductDetailViewModel>(selectedProduct, false, true);
+        }
+
+        public override async void Init(object data)
+        {
+            Products = (await _productService.GetAll()).ToObservableCollection();
+        }
+    }
+}
