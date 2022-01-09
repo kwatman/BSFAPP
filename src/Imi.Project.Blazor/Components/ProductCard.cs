b52ae3ccc8e80dcc75dde9fce6f053edba2126eb
AmauryDecru <amaury.decru@student.howest.be>
2022-01-09 23:28:07 +0100

    Link ProductsDetail Page to productCard

--- src/Imi.Project.Blazor/Components/ProductCard.cs
+++ src/Imi.Project.Blazor/Components/ProductCard.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Threading.Tasks;
 
 namespace Imi.Project.Blazor.Components
 {
     public partial class ProductCard
     {
+        [Parameter]
+        public Guid ProductId { get; set; }
         [Parameter]
         public string Name { get; set; }
         [Parameter]
