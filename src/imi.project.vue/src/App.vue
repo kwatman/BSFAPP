28f138f17029158aebcf2a02ba227d56eca3309b
AmauryDecru <amaury.decru@student.howest.be>
2022-01-13 15:11:17 +0100

    Init Vue App with Tailwind CSS

---
+++ src/imi.project.vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <nav>
+    </nav>
+  </header>
+  <main>
+    <HomePage/>
+  </main>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+main{
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+}
+</style>
