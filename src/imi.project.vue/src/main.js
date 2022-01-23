dc33aba80dcca226d5576925d7340f0559c8619a
AmauryDecru <amaury.decru@student.howest.be>
2022-01-23 18:24:51 +0100

    Add ProdductsPage with iteretable products call

--- src/imi.project.vue/src/main.js
+++ src/imi.project.vue/src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './mainstyle.css';
 import router from './router';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://localhost:5001';
 
 createApp(App)
-.use(router)
+.use(router, axios)
 .mount('#app');
