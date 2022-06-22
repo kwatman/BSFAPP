b5ee8a68243ee98a3850d0f3bf6eae759d68798e
AmauryDecru <amaury.decru@student.howest.be>
2022-06-22 10:58:22 +0200

    Add CombatRole model

---
+++ imi examenkans 2/Imi.Project.Herexamen.Api.Core/Models/CombatRole.cs
@@ -0,0 +1,8 @@
+namespace Imi.Project.Herexamen.Api.Core.Models
+{
+    public class CombatRole: Base
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
