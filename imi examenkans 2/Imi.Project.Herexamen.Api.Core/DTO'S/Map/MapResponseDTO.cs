404e3d8a590cae7b751fa69c23c7031ff10f14d2
AmauryDecru <amaury.decru@student.howest.be>
2022-07-04 16:24:15 +0200

    Add MapDTO's

---
+++ imi examenkans 2/Imi.Project.Herexamen.Api.Core/DTO'S/Map/MapResponseDTO.cs
@@ -0,0 +1,9 @@
+using Imi.Project.Herexamen.Api.Core.DTO_S.Base;
+
+namespace Imi.Project.Herexamen.Api.Core.DTO_S.Map
+{
+    public class MapResponseDTO : BaseDTO
+    {
+        public string Name { get; set; }
+    }
+}
