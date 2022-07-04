3fedf29974aa9ad5e0b74922e28f3f6512061627
AmauryDecru <amaury.decru@student.howest.be>
2022-07-04 11:37:10 +0200

    Add IOperationRepository.cs

--- imi examenkans 2/Imi.Project.Herexamen.Api.Core/Interfaces/Repositories/IOperationRepository.cs
+++ imi examenkans 2/Imi.Project.Herexamen.Api.Core/Interfaces/Repositories/IOperationRepository.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Imi.Project.Herexamen.Api.Core.Models;
+
 namespace Imi.Project.Herexamen.Api.Core.Interfaces.Repositories
 {
-    public interface IOperationRepository
+    public interface IOperationRepository : IBaseRepository<Operation>
     {
-        
+        Task<IEnumerable<Operation>> GetByMapIdAsync(Guid mapId);
+        Task<IEnumerable<Operation>> SearchAsync(string searchString);
     }
 }
