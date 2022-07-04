edb6e3f7871b0f7d4ffeb156381724f5dafa4367
AmauryDecru <amaury.decru@student.howest.be>
2022-07-04 11:44:45 +0200

    Add IUserRepository.cs

---
+++ imi examenkans 2/Imi.Project.Herexamen.Api.Core/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Imi.Project.Herexamen.Api.Core.Models;
+
+namespace Imi.Project.Herexamen.Api.Core.Interfaces.Repositories
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        Task<IEnumerable<User>> GetByCombatRoleIdAsync(Guid combatRoleId);
+        Task<IEnumerable<Operation>> SearchAsync(string searchString);
+    }
+}
