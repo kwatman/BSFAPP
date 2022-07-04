b6e64dd4f3298443ce3a01aefede7c0cbdd64990
AmauryDecru <amaury.decru@student.howest.be>
2022-07-04 13:47:33 +0200

    Implement ICombatRoleRepository.cs into CombatROleRepository.cs

--- imi examenkans 2/Imi.Project.Herexamen.Api.Infrastructure/Repositories/CombatRoleRepository.cs
+++ imi examenkans 2/Imi.Project.Herexamen.Api.Infrastructure/Repositories/CombatRoleRepository.cs
@@ -1,7 +1,14 @@
+using Imi.Project.Herexamen.Api.Core.Interfaces.Repositories;
+using Imi.Project.Herexamen.Api.Core.Models;
+using Imi.Project.Herexamen.Api.Infrastructure.Data;
+
 namespace Imi.Project.Herexamen.Api.Infrastucture.Repositories
 {
-    public class CombatRoleRepository
+    public class CombatRoleRepository : BaseRepository<CombatRole>, ICombatRoleRepository
     {
-        
+        public CombatRoleRepository(AppDbContext ctx) : base(ctx)
+        {
+
+        }
     }
 }
