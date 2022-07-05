e0fa39a41c09da5d5eb1bc20753b5790a349ce91
AmauryDecru <amaury.decru@student.howest.be>
2022-07-05 13:31:34 +0200

    Add CombatRoleDTO's

---
+++ imi examenkans 2/Imi.Project.Herexamen.Api.Core/DTO'S/CombatRole/CombatRoleResponseDTO.cs
@@ -0,0 +1,10 @@
+using Imi.Project.Herexamen.Api.Core.DTO_S.Base;
+
+namespace Imi.Project.Herexamen.Api.Core.DTO_S.CombatRole
+{
+    public class CombatRoleResponseDTO : BaseDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ imi examenkans 2/Imi.Project.Herexamen.Api.Core/DTO'S/Operation/OperationResponseDTO.cs
@@ -0,0 +1,7 @@
+namespace Imi.Project.Herexamen.Api.Core.DTO_S.Operation
+{
+    public class OperationResponseDTO
+    {
+        
+    }
+}
