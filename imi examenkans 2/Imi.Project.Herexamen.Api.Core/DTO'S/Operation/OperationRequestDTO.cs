341cee253d729e5d32e70d0893a258ade4eb7a6e
AmauryDecru <amaury.decru@student.howest.be>
2022-07-05 13:49:43 +0200

    Add OperationDTO's

--- imi examenkans 2/Imi.Project.Herexamen.Api.Core/DTO'S/Operation/OperationRequestDTO.cs
+++ imi examenkans 2/Imi.Project.Herexamen.Api.Core/DTO'S/Operation/OperationRequestDTO.cs
@@ -1,15 +1,14 @@
 using System;
 using Imi.Project.Herexamen.Api.Core.DTO_S.Base;
-using Imi.Project.Herexamen.Api.Core.DTO_S.Map;
 
 namespace Imi.Project.Herexamen.Api.Core.DTO_S.Operation
 {
-    public class OperationResponseDTO : BaseDTO
+    public class OperationRequestDTO : BaseDTO
     {
         public string CodeName { get; set; }
         public string Sitrep { get; set; }
         public DateTime ZeroHour { get; set; }
-        public MapResponseDTO Map { get; set; }
+        public Guid MapId { get; set; }
         // Participants
     }
 }
