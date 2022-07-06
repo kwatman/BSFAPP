59f6493958843ca03a33455ef073ecd283e2902a
AmauryDecru <amaury.decru@student.howest.be>
2022-07-06 16:42:39 +0200

    Add CombatRoleService.cs with mappings and ServiceResponse wrapper objects

---
+++ imi examenkans 2/Imi.Project.Herexamen.Api.Core/Interfaces/Services/ICombatRoleService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Imi.Project.Herexamen.Api.Core.DTO_S.CombatRole;
+using Imi.Project.Herexamen.Api.Core.Models;
+
+namespace Imi.Project.Herexamen.Api.Core.Interfaces.Services
+{
+    public interface ICombatRoleService
+    {
+        Task<ServiceResponse<IEnumerable<CombatRoleResponseDTO>>> GetAllCombatRoles();
+        Task<ServiceResponse<CombatRoleResponseDTO>> GetCombatRoleById(Guid combatRoleId);
+        Task<ServiceResponse<CombatRoleResponseDTO>> CreateCombatRole(CombatRoleRequestDTO request);
+    }
+}
