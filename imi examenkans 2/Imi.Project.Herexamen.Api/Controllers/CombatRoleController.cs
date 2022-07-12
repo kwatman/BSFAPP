fa3fe84f8a379f5bd89ba3bd755874e7333ee767
AmauryDecru <amaury.decru@student.howest.be>
2022-07-12 13:09:35 +0200

    Add MapController with all HTTP Methods

---
+++ imi examenkans 2/Imi.Project.Herexamen.Api/Controllers/CombatRoleController.cs
@@ -0,0 +1,7 @@
+namespace Imi.Project.Herexamen.Api.Controllers
+{
+    public class CombatRoleController
+    {
+        
+    }
+}
--- imi examenkans 2/Imi.Project.Herexamen.Api/Controllers/MapController.cs
+++ imi examenkans 2/Imi.Project.Herexamen.Api/Controllers/MapController.cs
@@ -1,13 +1,85 @@
+using System;
+using System.Threading.Tasks;
+using Imi.Project.Herexamen.Api.Core.DTO_S.Map;
+using Imi.Project.Herexamen.Api.Core.Interfaces.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Imi.Project.Herexamen.Api.Controllers
 {
-    public class MapController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MapController : ControllerBase
     {
-        // GET
-        public IActionResult Index()
+        protected readonly IMapService _mapService;
+
+        public MapController(IMapService mapService)
         {
-            return View();
+            _mapService = mapService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _mapService.GetAllMaps();
+
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var response = await _mapService.GetMapById(id);
+
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(MapRequestDTO request)
+        {
+            var response = await _mapService.CreateMap(request);
+
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(MapRequestDTO request)
+        {
+            var response = await _mapService.UpdateMap(request);
+
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var response = await _mapService.DeleteMap(id);
+
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
         }
     }
 }
