7d076e22530c5e864a349bfda82b1b0be58caf24
AmauryDecru <amaury.decru@student.howest.be>
2022-08-02 20:58:05 +0200

    Add AuthRepository with PasswordHasher, UsernameExist checker and Register method

--- imi examenkans 2/Imi.Project.Herexamen.Api.Infrastructure/Repositories/AuthRepository.cs
+++ imi examenkans 2/Imi.Project.Herexamen.Api.Infrastructure/Repositories/AuthRepository.cs
@@ -1,7 +1,66 @@
+using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Imi.Project.Herexamen.Api.Core.Interfaces.Repositories;
+using Imi.Project.Herexamen.Api.Core.Models;
+using Imi.Project.Herexamen.Api.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Imi.Project.Herexamen.Api.Infrastucture.Repositories
 {
-    public class AuthRepository
+    public class AuthRepository : IAuthRepository
     {
-        
+        private readonly AppDbContext _ctx;
+
+        public AuthRepository(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        private void HashPassword(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if (await _ctx.Users.AnyAsync(u => u.Username.ToLower() == username.ToLower()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<ServiceResponse<Guid>> Register(User user, string password)
+        {
+            ServiceResponse<Guid> response = new ServiceResponse<Guid>();
+
+            if (await UserExists(user.Username))
+            {
+                response.Success = false;
+                response.Message = "User already exists!";
+                return response;
+            }
+            else
+            {
+                HashPassword(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+
+                await _ctx.Users.AddAsync(user);
+                await _ctx.SaveChangesAsync();
+
+                response.Data = user.Id;
+                return response;
+            }
+        }
     }
 }
