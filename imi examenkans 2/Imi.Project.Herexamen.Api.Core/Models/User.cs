6ef33501d95797074d7d497d4f7433897a7e252a
AmauryDecru <amaury.decru@student.howest.be>
2022-08-02 18:49:33 +0200

    Add IAuthRepository interface

--- imi examenkans 2/Imi.Project.Herexamen.Api.Core/Models/User.cs
+++ imi examenkans 2/Imi.Project.Herexamen.Api.Core/Models/User.cs
@@ -9,6 +9,8 @@ namespace Imi.Project.Herexamen.Api.Core.Models
     public class User: Base
     {
         public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
         public bool SquadLeader { get; set; }
         public Guid CombatRoleId { get; set; }
         public CombatRole CombatRole  { get; set; }
